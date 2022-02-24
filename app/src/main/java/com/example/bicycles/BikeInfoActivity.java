36abed857a68baeddc6fb4614dcadaefe7850cfc
Jonathan Benson <jbensonamdg@gmail.com>
2022-02-24 11:36:08 -0600

    Added main activity initialization functionality

--- app/src/main/java/com/example/bicycles/BikeInfoActivity.java
+++ app/src/main/java/com/example/bicycles/BikeInfoActivity.java
@@ -2,13 +2,54 @@ package com.example.bicycles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class BikeInfoActivity extends AppCompatActivity {
+
+    private String bikeTitle, bikeDescription;
+
+
+    public BikeInfoActivity(String title, String description) {
+        this.bikeTitle = title;
+        this.bikeDescription = description;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bike_info);
+
+        TextView bikeTitleView = this.findViewById(R.id.bikeTitle);
+        bikeTitleView.setText(this.bikeTitle);
+
+        TextView bikeDescriptionView = this.findViewById(R.id.bikeDescription);
+        bikeDescriptionView.setText(this.bikeDescription);
+
+        Button backButton = this.findViewById(R.id.backButton);
+
+        BikeInfoActivity currentActivity = this;
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentActivity.goBack();
+            }
+        });
+
+    }
+
+    public void goBack() {
+
+        Intent previousIntent = this.getIntent();
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        intent.putExtra("mainActivityState", previousIntent.getStringExtra("mainActivityState"));
+
+        this.startActivity(intent);
     }
 }
--- app/src/main/java/com/example/bicycles/MainActivity.java
+++ app/src/main/java/com/example/bicycles/MainActivity.java
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +38,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        this.loadFragment(new ListViewFragment());
+        Intent intent = this.getIntent();
+
+        if (intent.getStringExtra("mainActivityState") == null ||intent.getStringExtra("mainActivityState") == "listView")
+            this.loadFragment(new ListViewFragment());
+        else
+            this.loadFragment(new RecycleViewFragment());
 
     }
 
     private void loadFragment(Fragment fragment) {
+
         this.getSupportFragmentManager().beginTransaction()
                 .add(R.id.fragmentContainerView, fragment)
                 .commit();
