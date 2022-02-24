5bdd858def6726669e337a826d7a794444d58f70
Jonathan Benson <jbensonamdg@gmail.com>
2022-02-24 10:55:43 -0600

    Added BikeInfoActivity

---
+++ app/src/main/java/com/example/bicycles/BikeInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.bicycles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BikeInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bike_info);
+    }
+}
